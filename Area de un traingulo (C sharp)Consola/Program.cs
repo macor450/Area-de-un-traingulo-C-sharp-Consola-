39cab8c8df37f28a9f74857c8be9d71fd9e4e1d5
Martin Cortez <i20050008@monclova.tecnm.mx>
2023-09-29 07:39:23 -0600

    Initial commit

---
+++ Area de un traingulo (C sharp)Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_de_un_traingulo__C_sharp_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bases = 10; int altura = 5;
+            int respuesta = bases * altura / 2;
+            Console.WriteLine(respuesta);
+            Console.ReadLine();
+        }
+    }
+}
